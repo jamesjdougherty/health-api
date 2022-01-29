0090d3c758ea46785bda1943d3a1a0a0024c5bde
James Dougherty <jjdougherty89@gmail.com>
2022-01-29 11:52:08 -0500

    Remove test file

--- src/main/java/com/doughdev/healthapi/HealthApiApplication.java
+++ src/main/java/com/doughdev/healthapi/HealthApiApplication.java
@@ -8,6 +8,7 @@ public class HealthApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthApiApplication.class, args);
+
 	}
 
 }
--- src/test/java/com/doughdev/healthapi/HealthApiApplicationTests.java
+++ src/test/java/com/doughdev/healthapi/HealthApiApplicationTests.java
@@ -1,20 +1,13 @@
 package com.doughdev.healthapi;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class HealthApiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String author = "James";
-
-		Assertions.assertEquals(author, "James");
 	}
 
 }
